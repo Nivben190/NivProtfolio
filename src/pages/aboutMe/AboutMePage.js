e450a0f4fefd3ce1ff4cae97a6c70129ba6c43ae
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-05 18:33:29 +0300

    changed about me p

--- src/pages/aboutMe/AboutMePage.js
+++ src/pages/aboutMe/AboutMePage.js
@@ -42,14 +42,15 @@ font-size: larger;
         <AboutMe>About Me</AboutMe>
         <AboutMeP>i’m Niv ,a passionate full-stack developer.</AboutMeP>
         <AboutMeP>
-        Since I was a girl , I’ve always loved computers , and I started programming as a teenager .
+        Since I was a little kid  , I’ve always loved computers , and I started programming in high school at robotics trend .
         </AboutMeP>
-        <AboutMeP>After high school , I followed my dreams and studied software engineering and development .</AboutMeP>
+        <AboutMeP>After high school , I Join the militry to the nahal brigade and served as a soldier for 3.2 years.
+         After the militry  I followed my dreams and studied software engineering and development At Sela College .</AboutMeP>
         <AboutMeP>Finally , I could use my logical side to code and my creative side to design .</AboutMeP>
-        <AboutMeP>These days my time is spent coding , designing , learning and researching .Out of the office you’ll find me playing chess , solving a Rubik's cube , and listening to music</AboutMeP>
+        <AboutMeP>These days my time is spent coding , designing , learning and researching .Out of the office you’ll find me playing Basketball , Playing with my dog , and listening to a lot of music</AboutMeP>
 
         <CVBtn
-            txt="Download CV"
+            txt="Look At My  Resume"
         />
         <a href="https://wa.link/35vlg3"><ButtonComponent
              txt="  Contact Me"
