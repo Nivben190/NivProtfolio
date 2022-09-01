6bb3ab44e13681ec7a68f22d4b8b1a8307f6ec03
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-09-01 10:28:14 +0300

    docker added

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const publicPath = path.join(`${__dirname}/myprotfolio`, '..', 'public');
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const publicPath = path.join(`${__dirname}/myprotfolio`, '..', 'public');
+// const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(publicPath, 'index.html'));
+//  });
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
- });
+// app.listen(port, () => {
+//     console.log(`Server is up on port ${port}!`);
+//  });
